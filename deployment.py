0cb7e56a67d8016c9f5c18f7c1ef2cff2e272581
Harissh <harisshkrishna@gmail.com>
2025-07-26 02:57:38 +0530

    Grad io implementation

--- deployment.py
+++ deployment.py
@@ -3,21 +3,15 @@ import numpy as np
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 
-# Load your trained model
-model = load_model('maskdedector.keras')
 
-# Function to make predictions and return image + result
+model = load_model('maskdedector.keras')
 def predict_mask(img):
-    # Prediction
     img_resized = img.resize((64, 64))
     arr = image.img_to_array(img_resized)
     arr = np.expand_dims(arr, axis=0)
     prediction = model.predict(arr)
-    # Class logic
     result = "This person is not wearing a mask !!!! " if prediction[0][0] > 0.5 else "This person is wearing a mask !!!!"
-    return result, img  # Return BOTH result string and original uploaded image
-
-# Gradio interface setup
+    return result, img 
 iface = gr.Interface(
     fn=predict_mask,
     inputs=gr.Image(type="pil", label="Upload Image"),
